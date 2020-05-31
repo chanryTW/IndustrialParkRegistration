8b784fff186036f8995c1c77151f4dff5e476fb4
chanryTW <xd9440206@gmail.com>
2020-05-31 16:51:47 +0800

    初版 1.1.0

--- main.js
+++ main.js
@@ -40,6 +40,19 @@ $(function() {
 
             // 報名場次
             var session = $('#session_select').val() || '未填寫';
+            // 判斷日期與地址
+            var date = "";
+            var address = "";
+            if (session=="北") {
+                date = "109年6月17日(三)";
+                address = "南港軟體園區國際會議中心A棟2樓會議室(台北市南港區三重路19-10號A棟2樓)";
+            } else if (session=="中") {
+                date = "109年6月24日(三)";
+                address = "蓮潭國際會館國際二廳會議室(高雄市左營區崇德路801號)";
+            } else if (session=="南") {
+                date = "109年7月7日(二)";
+                address = "台中工業區服務中心人才培訓中心會議室(台中工業區27路17號3樓）";
+            }
 
             // 備註
             var remarks = $('#remarks_textarea').val() || '未填寫';
@@ -65,23 +78,29 @@ $(function() {
                     console.log("表單傳送成功");
 
                     // 寄送確認信
-                    // emailjs.init("user_B7Nb9TpDYHuihhajCCfIq");
-                    // let templateParams = {
-                    //     "userMail": userMail,
-                    //     "userName": userName,
-                    //     "position": position
-                    // }
-                    // var service_id = "default_service";
-                    // var template_id = "industrialparkregistration";
-                    // emailjs.send(service_id, template_id, templateParams)
-                    //     .then((response) => {
-                    //     console.log('確認信寄送成功', response.status, response.text);
+                    emailjs.init("user_B7Nb9TpDYHuihhajCCfIq");
+                    let templateParams = {
+                        "userMail": userMail,
+                        "userName": userName,
+                        "section": section,
+                        "position": position,
+                        "date": date,
+                        "address": address
+                    }
+                    var service_id = "default_service";
+                    var template_id = "industrialparkregistration";
+                    emailjs.send(service_id, template_id, templateParams)
+                    .then((response) => {
+                        console.log('確認信寄送成功', response.status, response.text);
                         // 跳轉完成頁面
-
-                    //     })
-                    //     .catch((error) => {
-                    //     console.log('確認信寄送失敗', error);
-                    //     })
+                        $('#mainPage').addClass("d-none");
+                        $('#finalPage').removeClass("d-none");
+                    })
+                    .catch((error) => {
+                        console.log('確認信寄送失敗', error);
+                        alert("確認信寄送失敗，請重新填寫或連繫承辦人員，非常抱歉。");
+                        window.location.reload();
+                    })
                 }
             });
         } else {
